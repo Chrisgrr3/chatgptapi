191bcc3b6968daa3e9bdea166070f63800ef2907
Christopher Guerrero <chrisgrr3@gmail.com>
2023-01-24 15:50:13 -0700

    Creating an OpenAIApi instance and setting up connection to API

--- index.js
+++ index.js
@@ -6,18 +6,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const OpenAI = require('openai');
+const {Configuration, OpenAIApi} = OpenAI;
+
 const app = express();
 const port = 3001;
+
+const configuration = new Configuration({
+    organization: "org-g8BaSLse4k8o7zqdCCmaXRqu",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // Telling the express app to use body parser and cors.
 app.use(bodyParser.json());
 app.use(cors());
 
 // Defining the '/' route.
-app.post('/', (req, res) => {
-    res.json({
-        message: "Hello World!"
+app.post('/', async(req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
     });
+    console.log(response.data);
+    if(response.data) {
+        if(response.data.choices) {
+            res.json({
+                message: response.data.choices[0].text
+            });
+        }
+    }
 });
 
 app.listen(port, () => {
