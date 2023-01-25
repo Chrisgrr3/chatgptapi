2745a917892db29def3da106642588a3e1ec6dc8
Christopher Guerrero <chrisgrr3@gmail.com>
2023-01-25 14:29:39 -0700

    Removed prompt engineering in order to make app answer any question

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/', async(req, res) => {
         // prompt: message,
         // Here, we can take advantage of prompt engineering
         // from the backend.
-        prompt: `Answer by providing an explanation of the code.${message}?`, // We insert a question mark just in case, so that ChatGPT doesn't do it for us.
+        prompt: message, // We insert a question mark just in case, so that ChatGPT doesn't do it for us.
         max_tokens: 4000, // Don't overdo this and use too much credit.
         temperature: 0,
     });
